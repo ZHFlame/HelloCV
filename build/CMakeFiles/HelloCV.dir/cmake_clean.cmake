84de8e036f3b0d320430652540e8f6da0e2d0967
Lychee <lzhhao6@outlook.com>
2025-10-25 13:37:25 +0800

    基于OpenCV的红绿灯识别

--- build/CMakeFiles/HelloCV.dir/cmake_clean.cmake
+++ build/CMakeFiles/HelloCV.dir/cmake_clean.cmake
@@ -1,8 +1,8 @@
 file(REMOVE_RECURSE
   "CMakeFiles/HelloCV.dir/main.cpp.o"
   "CMakeFiles/HelloCV.dir/main.cpp.o.d"
-  "CryptoTool"
-  "CryptoTool.pdb"
+  "TrafficLight_Detect"
+  "TrafficLight_Detect.pdb"
 )
 
 # Per-language clean rules from dependency scanning.
